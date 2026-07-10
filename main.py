bd164dddd81c7bab92723eb9eac33a2c723405ff
Stu67nt <tanzilchowdhury2014@gmail.com>
2026-07-10 22:13:31 +0100

    Optimised runtime calculations significantly through numpy vecotrisation.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 import keyboard
 import time
 import scipy
+import numpy
 
 # /execute in minecraft:overworld run tp @s 235.84 69.00 -195.63 -134.79 -31.61
 
@@ -11,34 +12,36 @@ import scipy
 # /execute in minecraft:overworld run tp @s 1960.78 68.00 -1301.01 132.21 -31.74
 """
 TODO:
-optimise precompute (turns out its alr pretty decent)
-
+Coordinate snapping /
+Nether coordines /
+Ring additions /
 """
 
-RING_REGIONS = [[1280, 2816, 3],   # Ring 1 - 3 Strongholds
-				[4352, 5888, 6],   # Ring 2 - 6 Strongholds
-				[7424, 8960, 10],  # Ring 3 - 10 Strongholds
-				[10496, 12032, 15],  # Ring 4 - 15 Strongholds
-				[13568, 15104, 21],  # Ring 5 - 21 Strongholds
-				[16640, 18176, 28],  # Ring 6 - 28 Strongholds
-				[19712, 21248, 36],  # Ring 7 - 36 Strongholds
-				[22784, 24320, 9]  # Ring 8 - 9 Strongholds
+RING_REGIONS = [
+	[1280, 2816, 3],  # Ring 1 - 3 Strongholds
+	[4352, 5888, 6],  # Ring 2 - 6 Strongholds
+	[7424, 8960, 10],  # Ring 3 - 10 Strongholds
+	[10496, 12032, 15],  # Ring 4 - 15 Strongholds
+	[13568, 15104, 21],  # Ring 5 - 21 Strongholds
+	[16640, 18176, 28],  # Ring 6 - 28 Strongholds
+	[19712, 21248, 36],  # Ring 7 - 36 Strongholds
+	[22784, 24320, 9]  # Ring 8 - 9 Strongholds
 				]
 
 def integrand(x_1, a):
-	val = (a * x_1) / (15 * math.sqrt(2))
+	val = (a * x_1) / (15 * numpy.sqrt(2))
 	if val <= -1 or val >= 1:
 		return 0
-	return math.pow(1 + val, 4.5) * math.pow(1 - val,4.5)
+	return numpy.pow(1 + val, 4.5) * numpy.pow(1 - val,4.5)
 
 def diff_ring_integrand(chunk_angle, chunk_displacement, min_dist, max_dist):
-	denom = math.sin(-chunk_displacement)
+	denom = numpy.sin(-chunk_displacement)
 	# prevent divide by 0 errors
 	if round(denom, 8) == 0:
 		return 0
 
-	lb = chunk_displacement * math.sin(chunk_angle) / denom
-	ub = chunk_displacement * math.sin(math.pi+chunk_angle) / denom
+	lb = chunk_displacement * numpy.sin(chunk_angle) / denom
+	ub = chunk_displacement * numpy.sin(numpy.pi+chunk_angle) / denom
 
 	if lb > ub:
 		ub, lb = lb, ub
@@ -48,10 +51,10 @@ def diff_ring_integrand(chunk_angle, chunk_displacement, min_dist, max_dist):
 		if R > max_dist: return 1.0
 		return (R - min_dist)/(max_dist - min_dist)
 
-	return (radial_cdf(ub) - radial_cdf(lb))/(2*math.pi)
+	return (radial_cdf(ub) - radial_cdf(lb))/(2*numpy.pi)
 
 def precompute_chance_calc(x_coord, z_coord, reigon_num):
-	displacement = math.sqrt(pow(x_coord,2)+pow(z_coord, 2))
+	displacement = numpy.sqrt(pow(x_coord,2)+pow(z_coord, 2))
 	p_closest = 1
 	for i, region in enumerate(RING_REGIONS):
 		if i != reigon_num:
@@ -62,7 +65,7 @@ def precompute_chance_calc(x_coord, z_coord, reigon_num):
 				p_further = 1.0
 			else:
 				p_further = (displacement - min_dist) / (max_dist - min_dist)
-			p_closer = math.pow(1-p_further, strng_count)
+			p_closer = numpy.pow(1-p_further, strng_count)
 			p_closest *= p_closer
 	return p_closest
 
@@ -71,32 +74,26 @@ def precompute_g_set():
 	i = 0
 	for region in RING_REGIONS:
 		raw_weight_total = 0
-		ring_set = {"angle_density": 0,
-					"joint_desnity": 0,
-					"chunk_info": []}
+		ring_set = [0, 0, []]
 		min_dist, max_dist = region[0], region[1]
-		ring_set["angle_density"] = scipy.integrate.quad(integrand, (-15 * math.sqrt(2)) / min_dist, (15 * math.sqrt(2)) / min_dist, args=(min_dist))
-		ring_set["joint_desnity"] = 1/(2*math.pi*(max_dist-min_dist))
+		ring_set[0] = scipy.integrate.quad(integrand, (-15 * numpy.sqrt(2)) / min_dist, (15 * numpy.sqrt(2)) / min_dist, args=(min_dist))
+		ring_set[1] = 1/(2*numpy.pi*(max_dist-min_dist))
 		for x in range(-max_dist, max_dist + 1, 16):
 			x_coord = x - (x % 16) + 8
 			for z in range(-max_dist, max_dist + 1, 16):
 				z_coord = z - (z % 16) + 8
-				displacement = math.sqrt(pow(x_coord, 2) + pow(z_coord, 2))
+				displacement = numpy.sqrt(pow(x_coord, 2) + pow(z_coord, 2))
 				if min_dist <= displacement <= max_dist:
 					closest_factor = precompute_chance_calc(x_coord, z_coord, i)
-					angle = math.atan2(-x_coord, z_coord)
-					raw_weight = ring_set["joint_desnity"] * (256 / displacement) * closest_factor
-					ring_set["chunk_info"].append({"coords": [x_coord, z_coord],
-												   "displacment": displacement,
-												   "angle": angle,
-												   "weight": raw_weight})
-					raw_weight_total += raw_weight
+					angle = numpy.atan2(-x_coord, z_coord)
+					raw_weight = ring_set[1] * (256 / displacement) * closest_factor
+					ring_set[2].append((x_coord, z_coord, displacement, angle, raw_weight))
+		ring_set[2] = numpy.array(ring_set[2], dtype=[("x_coord", "f8"), ("z_coord", numpy.float64), ("displacement", numpy.float64), ("angle", numpy.float64), ("raw_weight", numpy.float64)])
+		raw_weight_total = numpy.sum(ring_set[2]["raw_weight"])
 		if raw_weight_total > 0:
-			for chunk in ring_set["chunk_info"]:
-				chunk["weight"] = region[2]*((chunk["weight"])/raw_weight_total)
+			ring_set[2]["raw_weight"] = region[2]*(ring_set[2]["raw_weight"]/raw_weight_total)
 		else:
-			for chunk in ring_set["chunk_info"]:
-				chunk["weight"] = 0
+			ring_set[2]["raw_weight"] = 0
 		i += 1
 		G_set.append(ring_set)
 		print(f"{i}/{len(RING_REGIONS)}")
@@ -110,27 +107,20 @@ def find_probablilty(player_pos: tuple, g_set, strd_dev, throws=1):
 	:param throws:
 	:return:
 	"""
-	player_angle = math.radians(player_pos[2])
-	strd_dev = math.radians(strd_dev)
+	player_angle = numpy.radians(player_pos[2])
+	strd_dev = numpy.radians(strd_dev)
 	i = 0
 	total_prob = 0
 	for reigon in g_set:
-		for chunk in reigon["chunk_info"]:
-			optimal_angle = math.atan2(-(chunk["coords"][0]-player_pos[0]), chunk["coords"][1]-player_pos[1])
-			angle_diff = player_angle - optimal_angle
-			if angle_diff > math.pi:
-				angle_diff = angle_diff - 2*math.pi
-			elif angle_diff < -math.pi:
-				angle_diff = 2*math.pi + angle_diff
-			chance = (1/(strd_dev*math.sqrt(2 * math.pi)))*math.exp(-(pow(angle_diff, 2))/(2*pow(strd_dev, 2)))
-			displacement = math.sqrt(pow(chunk["coords"][0]-player_pos[0], 2) + pow(chunk["coords"][1]-player_pos[1], 2))
-			chunk["weight"] *= chance
-			total_prob += chunk["weight"]
+		optimal_angle = numpy.atan2(-(reigon[2]["x_coord"]-player_pos[0]), reigon[2]["z_coord"]-player_pos[1])
+		angle_diff = ((-1*optimal_angle)+player_angle+numpy.pi)%(2*numpy.pi)-numpy.pi
+		chance = (1/(strd_dev*numpy.sqrt(2 * numpy.pi)))*numpy.exp(-(numpy.pow(angle_diff, 2))/(2*pow(strd_dev, 2)))
+		reigon[2]["raw_weight"] *= chance
+		total_prob += numpy.sum(reigon[2]["raw_weight"])
 		i+=1
 
 	for reigon in g_set:
-		for chunk in reigon["chunk_info"]:
-			chunk["weight"] /= total_prob
+		reigon[2]["raw_weight"] /= total_prob
 	return g_set
 
 def extract_best(g_set):
@@ -138,11 +128,11 @@ def extract_best(g_set):
 	x = 0
 	z = 0
 	for reigon in g_set:
-		for chunk in reigon["chunk_info"]:
-			if chunk["weight"] > best_val:
-				best_val = chunk["weight"]
-				x = chunk["coords"][0]
-				z = chunk["coords"][1]
+		i = numpy.argmax(reigon[2]["raw_weight"])
+		if best_val < numpy.max(reigon[2][i]["raw_weight"]):
+			best_val = reigon[2][i]["raw_weight"]
+			x = reigon[2][i]["x_coord"]
+			z = reigon[2][i]["z_coord"]
 	return x,z, best_val
 
 def read_clipboard():
@@ -162,7 +152,7 @@ def parse_result(command):
 	return x,y,z,yaw,pitch
 
 def get_gradient(pitch):
-	return -1/math.tan(math.radians(pitch))
+	return -1/numpy.tan(numpy.radians(pitch))
 
 def get_constant(x, z, gradient):
 	return z-(gradient*x)
@@ -178,7 +168,7 @@ def nether_coords(x, z):
 	return x//8, z//8
 
 def validate_result(x, z):
-	displacement = math.sqrt(x*x + z*z)
+	displacement = numpy.sqrt(x*x + z*z)
 	for i, ring in enumerate(RING_REGIONS):
 		if ring[0] < displacement < ring[1]:
 			return i+1
@@ -235,7 +225,7 @@ def main_probabilistic():
 			count += 1
 			c_x, c_z, chance= extract_best(g_set)
 			print(time.time() - t1)
-			print(f"Overworld coords: {c_x, c_z}")
+			print(f"Overworld coords: {int(c_x), int(c_z)}")
 			print(f"Nether coords: {nether_coords(c_x, c_z)}")
 			print(chance)
 			ring = validate_result(c_x, c_z)
