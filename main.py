227c01b9e588578b3eb044a7deca26247e907730
Stu67nt <tanzilchowdhury2014@gmail.com>
2026-07-19 12:54:17 +0100

    Fixed a few issues with finding best 5 candidates and prob calc

--- main.py
+++ main.py
@@ -12,27 +12,29 @@ import numpy
 # /execute in minecraft:overworld run tp @s 1960.78 68.00 -1301.01 132.21 -31.74
 """
 TODO:
-Coordinate snapping /
-Nether coordines /
-Ring additions /
-"""
 
-"""	[10496, 12032, 15],  # Ring 4 - 15 Strongholds
-	[13568, 15104, 21],  # Ring 5 - 21 Strongholds
-	[16640, 18176, 28],  # Ring 6 - 28 Strongholds
-	[19712, 21248, 36],  # Ring 7 - 36 Strongholds
-	[22784, 24320, 9]  # Ring 8 - 9 Strongholds"""
+"""
 
 RING_REGIONS = [
 	[1280, 2816, 3],  # Ring 1 - 3 Strongholds
 	[4352, 5888, 6],  # Ring 2 - 6 Strongholds
-	[7424, 8960, 10],  # Ring 3 - 10 Strongholds
+	[7424, 8960, 10]  # Ring 3 - 10 Strongholds
 	[10496, 12032, 15],  # Ring 4 - 15 Strongholds
 	[13568, 15104, 21],  # Ring 5 - 21 Strongholds
 	[16640, 18176, 28],  # Ring 6 - 28 Strongholds
 	[19712, 21248, 36],  # Ring 7 - 36 Strongholds
 	[22784, 24320, 9]  # Ring 8 - 9 Strongholds
 				]
+
+def surrounding_rings(player_displacement):
+	for_back_rings = []
+	for i, ring in enumerate(RING_REGIONS):
+		if (i == 0 and player_displacement <= ring[0]) or (i == 7 and player_displacement >= ring[1]):
+			return [RING_REGIONS[i]]
+		elif player_displacement <= ring[0] or player_displacement >= ring[1]:
+			for_back_rings.append(RING_REGIONS[i])
+
+
 
 def integrand(x_1, a):
 	val = (a * x_1) / (15 * numpy.sqrt(2))
@@ -57,24 +59,7 @@ def diff_ring_integrand(chunk_angle, player_angle, player_displacement, player_c
 	def radial_cdf(displacement_arr):
 		displacement_arr = numpy.clip(displacement_arr, min_dist, max_dist)
 		return (displacement_arr-min_dist)/(max_dist-min_dist)
-
-	return (radial_cdf(ub) - radial_cdf(lb))/(2*math.pi)
-
-def precompute_chance_calc(x_coord, z_coord, reigon_num):
-	displacement = math.sqrt(pow(x_coord,2)+pow(z_coord, 2))
-	p_closest = 1
-	for i, region in enumerate(RING_REGIONS):
-		if i != reigon_num:
-			min_dist, max_dist, strng_count = region[0], region[1], region[2]
-			if displacement <= min_dist:
-				p_further = 0.0
-			elif displacement >= max_dist:
-				p_further = 1.0
-			else:
-				p_further = (displacement - min_dist) / (max_dist - min_dist)
-			p_closer = math.pow(1-p_further, strng_count)
-			p_closest *= p_closer
-	return p_closest
+	return (radial_cdf(t_ub) - radial_cdf(t_lb))/(2*math.pi)
 
 def precompute_g_set():
 	G_set = []
@@ -83,17 +68,19 @@ def precompute_g_set():
 		raw_weight_total = 0
 		ring_set = [0, 0, []]
 		min_dist, max_dist = region[0], region[1]
+		# Beta distribution integral
 		ring_set[0] = scipy.integrate.quad(integrand, (-15 * math.sqrt(2)) / min_dist, (15 * math.sqrt(2)) / min_dist, args=(min_dist))
+		# WTF ARE YOU
 		ring_set[1] = 1/(2*math.pi*(max_dist-min_dist))
+
 		for x in range(-max_dist, max_dist + 1, 16):
 			x_coord = x - (x % 16) + 8
 			for z in range(-max_dist, max_dist + 1, 16):
 				z_coord = z - (z % 16) + 8
 				displacement = math.sqrt(pow(x_coord, 2) + pow(z_coord, 2))
 				if min_dist <= displacement <= max_dist:
-					closest_factor = precompute_chance_calc(x_coord, z_coord, i)
 					angle = math.atan2(-x_coord, z_coord)
-					raw_weight = ring_set[1] * (256 / displacement) * closest_factor
+					raw_weight = ring_set[0][0] * (256 / displacement) * ring_set[1]
 					ring_set[2].append((x_coord, z_coord, displacement, angle, raw_weight))
 		ring_set[2] = numpy.array(ring_set[2], dtype=[("x_coord", "f8"), ("z_coord", numpy.float64), ("displacement", numpy.float64), ("angle", numpy.float64), ("raw_weight", numpy.float64)])
 		raw_weight_total = numpy.sum(ring_set[2]["raw_weight"])
@@ -118,7 +105,6 @@ def find_probablilty(player_pos: tuple, g_set, strd_dev, throws=1):
 	player_look_angle = math.radians(player_pos[2])
 	player_pos_angle = math.atan2(-player_pos[0], player_pos[1])
 	strd_dev = math.radians(strd_dev)
-	i = 0
 	total_prob = 0
 	for i, reigon in enumerate(g_set):
 		optimal_angle = numpy.atan2(-(reigon[2]["x_coord"]-player_pos[0]), reigon[2]["z_coord"]-player_pos[1])
@@ -130,40 +116,28 @@ def find_probablilty(player_pos: tuple, g_set, strd_dev, throws=1):
 			min_dist, max_dist, count = RING_REGIONS[j][0], RING_REGIONS[j][1], RING_REGIONS[j][2]
 			if i != j:
 				player_chunk_displacement_arr = numpy.sqrt(numpy.pow(reigon[2]["x_coord"]-player_pos[0], 2)+numpy.pow(reigon[2]["z_coord"]-player_pos[1], 2))
-				chance_closer_arr = scipy.integrate.quad_vec(diff_ring_integrand, 0, 2*math.pi, args=(player_pos_angle, player_displacement, player_chunk_displacement_arr, min_dist, max_dist))
+				chance_closer_arr = scipy.integrate.quad_vec(diff_ring_integrand, 0, 2*math.pi, args=(player_pos_angle, player_displacement, player_chunk_displacement_arr, min_dist, max_dist), limit=10)
 				placement_correction *= numpy.pow(1 - chance_closer_arr[0], count)
-
 		reigon[2]["raw_weight"] *= chance * placement_correction
 		total_prob += numpy.sum(reigon[2]["raw_weight"])
-		i+=1
 
 	for reigon in g_set:
 		reigon[2]["raw_weight"] /= total_prob
 	return g_set
 
 def extract_best(g_set):
-	best_i = [-1, -1]
-	best_lst = []
-	for count in range(0, 5):
-		best_val = -1
-		x = 0
-		z = 0
-		for j, reigon in enumerate(g_set):
-			i = numpy.argmax(reigon[2]["raw_weight"])
-			if best_val < numpy.max(reigon[2][i]["raw_weight"]):
-				best_val = float(reigon[2][i]["raw_weight"])
-				x = int(reigon[2][i]["x_coord"])
-				z = int(reigon[2][i]["z_coord"])
-				best_i = (j, i)
-		best_lst.append((x, z, best_val))
-		for i, reigon in enumerate(g_set):
-			if i == best_i[0]:
-				reigon[2] = numpy.delete(reigon[2], best_i[1])
-
-	print(best_lst)
-	x = best_lst[0][0]
-	z = best_lst[0][1]
-	best_val = best_lst[0][2]
+	flattened_gset = []
+	for reigon in g_set:
+		flattened_gset.append(reigon[2])
+	flattened_gset = numpy.concatenate(flattened_gset)
+	best_5_i = numpy.argsort(flattened_gset["raw_weight"], descending=True)[0:5]
+	best_coords = []
+	for i in best_5_i:
+		best_coords.append((int(flattened_gset[i]["x_coord"]), int(flattened_gset[i]["z_coord"]), int(flattened_gset[i]["displacement"]), float(numpy.degrees(flattened_gset[i]["angle"])), float(flattened_gset[i]["raw_weight"])))
+	print(best_coords)
+	x = best_coords[0][0]
+	z= best_coords[0][1]
+	best_val = best_coords[0][4]
 	return x,z, best_val
 
 def read_clipboard():
@@ -244,7 +218,7 @@ def main_probabilistic():
 	while True:
 		print("waiting one")
 		keyboard.wait("f3+c")
-		time.sleep(0.1)
+		time.sleep(0.02)
 		command1 = read_clipboard()
 
 		if command1 != "invalid":
