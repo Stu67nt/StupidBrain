eaa6da6b3ce7c84352fb0bd9c09bda1edfe54062
Stu67nt <tanzilchowdhury2014@gmail.com>
2026-07-20 20:31:22 +0100

    Implemented simpson integral method and surronding rings logic. Fixed a few issues with finding best 5 candidates.

--- main.py
+++ main.py
@@ -10,29 +10,42 @@ import numpy
 # Tst pos 1
 # /execute in minecraft:overworld run tp @s 1920.48 75.00 -1259.10 139.88 -31.30
 # /execute in minecraft:overworld run tp @s 1960.78 68.00 -1301.01 132.21 -31.74
+
+#test pos
+# /execute in minecraft:overworld run tp @s 15144.95 66.00 15039.22 -548.52 -31.60
+# /execute in minecraft:overworld run tp @s 15134.30 66.00 15041.20 171.86 171.86
+# /execute in minecraft:overworld run tp @s 15171.84 66.00 15041.59 170.39 170.39
 """
 TODO:
 
 """
 
 RING_REGIONS = [
-	[1280, 2816, 3],  # Ring 1 - 3 Strongholds
-	[4352, 5888, 6],  # Ring 2 - 6 Strongholds
-	[7424, 8960, 10]  # Ring 3 - 10 Strongholds
-	[10496, 12032, 15],  # Ring 4 - 15 Strongholds
-	[13568, 15104, 21],  # Ring 5 - 21 Strongholds
-	[16640, 18176, 28],  # Ring 6 - 28 Strongholds
-	[19712, 21248, 36],  # Ring 7 - 36 Strongholds
-	[22784, 24320, 9]  # Ring 8 - 9 Strongholds
+	[1280, 2816, 3],  # Ring 1
+	[4352, 5888, 6],  # Ring 2
+	[7424, 8960, 10],  # Ring 3
+	[10496, 12032, 15],  # Ring 4
+	[13568, 15104, 21],  # Ring 5
+	[16640, 18176, 28],  # Ring 6
+	[19712, 21248, 36],  # Ring 7
+	[22784, 24320, 9]  # Ring 8
 				]
 
 def surrounding_rings(player_displacement):
 	for_back_rings = []
+	max_ring = len(RING_REGIONS)-1
 	for i, ring in enumerate(RING_REGIONS):
-		if (i == 0 and player_displacement <= ring[0]) or (i == 7 and player_displacement >= ring[1]):
+		if (i == 0 and player_displacement <= ring[0]) or (i == max_ring and player_displacement >= ring[1]):
 			return [RING_REGIONS[i]]
-		elif player_displacement <= ring[0] or player_displacement >= ring[1]:
-			for_back_rings.append(RING_REGIONS[i])
+		elif ring[0] <= player_displacement <= ring[1]:
+			if i == 0:
+				return [RING_REGIONS[i], RING_REGIONS[i+1]]
+			elif i == max_ring:
+				return [RING_REGIONS[i-1], RING_REGIONS[i]]
+			else:
+				return [RING_REGIONS[i - 1], RING_REGIONS[i], RING_REGIONS[i + 1]]
+		if RING_REGIONS[i][1] <= player_displacement <= RING_REGIONS[i+1][0]:
+			return [RING_REGIONS[i], RING_REGIONS[i+1]]
 
 
 
@@ -42,24 +55,37 @@ def integrand(x_1, a):
 		return 0
 	return math.pow(1 + val, 4.5) * math.pow(1 - val,4.5)
 
-def diff_ring_integrand(chunk_angle, player_angle, player_displacement, player_chunk_displacement, min_dist, max_dist):
-	angle_gap = math.sin(player_angle-chunk_angle)
-	# prevent divside by 0 errors
-	if round(angle_gap, 8) == 0:
-		return 0
-	arg = numpy.clip((player_displacement/player_chunk_displacement) * angle_gap , -1, 1)
-	beta = numpy.asin(arg)
 
-	lb = player_chunk_displacement * numpy.sin(beta-(player_angle-chunk_angle))/angle_gap
-	ub = player_chunk_displacement * numpy.sin(math.pi-beta-(player_angle-chunk_angle))/angle_gap
+def diff_ring_integrand_simpson(player_angle, player_displacement, player_chunk_displacement, min_dist, max_dist):
+	BATCH_SIZE = 20000
+	DATA_POINTS = 11
+	split_add = 2*math.pi / DATA_POINTS  # Needs odd number of data points
+	angle_splits = numpy.array([0+split_add*i for i in range(0,DATA_POINTS)])
+	player_angle_diff = (player_angle-angle_splits)[:,None]
+	angle_gap = numpy.sin(player_angle_diff)
+	angle_gap = numpy.where(numpy.round(angle_gap, 8) == 0, 0, angle_gap)
+	chance = numpy.empty_like(player_chunk_displacement)
+	for start_i in range(0, len(player_chunk_displacement), BATCH_SIZE):
+		end_i = min(len(player_chunk_displacement), start_i+BATCH_SIZE)
+		player_chunk_displacement_bit = player_chunk_displacement[start_i: end_i][None, :]
 
-	t_ub = numpy.maximum(ub, lb)
-	t_lb = numpy.minimum(ub, lb)
+		# Solving for beta
+		arg = numpy.clip((player_displacement / player_chunk_displacement_bit) * angle_gap, -1, 1)
+		beta = numpy.asin(arg)
 
-	def radial_cdf(displacement_arr):
-		displacement_arr = numpy.clip(displacement_arr, min_dist, max_dist)
-		return (displacement_arr-min_dist)/(max_dist-min_dist)
-	return (radial_cdf(t_ub) - radial_cdf(t_lb))/(2*math.pi)
+		# FIx the (player-angle-angle_splits section)
+		# Solving for upper and lower bounds as defined in the paper
+		lb = player_chunk_displacement_bit * numpy.sin(beta - player_angle_diff) / angle_gap
+		ub = player_chunk_displacement_bit * numpy.sin(math.pi - beta - player_angle_diff) / angle_gap
+
+		# Correcting upper and lower bounds to prevent negative values
+		t_ub = numpy.clip(numpy.maximum(ub, lb), min_dist, max_dist)
+		t_lb = numpy.clip(numpy.minimum(ub, lb), min_dist, max_dist)
+
+		vals = (t_ub-t_lb)/(max_dist-min_dist)/(2*math.pi)
+		chance[start_i:end_i] = scipy.integrate.simpson(y=vals, x=angle_splits, axis=0)
+	return chance
+
 
 def precompute_g_set():
 	G_set = []
@@ -80,7 +106,7 @@ def precompute_g_set():
 				displacement = math.sqrt(pow(x_coord, 2) + pow(z_coord, 2))
 				if min_dist <= displacement <= max_dist:
 					angle = math.atan2(-x_coord, z_coord)
-					raw_weight = ring_set[0][0] * (256 / displacement) * ring_set[1]
+					raw_weight = ring_set[0][0] * (256 / displacement)
 					ring_set[2].append((x_coord, z_coord, displacement, angle, raw_weight))
 		ring_set[2] = numpy.array(ring_set[2], dtype=[("x_coord", "f8"), ("z_coord", numpy.float64), ("displacement", numpy.float64), ("angle", numpy.float64), ("raw_weight", numpy.float64)])
 		raw_weight_total = numpy.sum(ring_set[2]["raw_weight"])
@@ -92,6 +118,7 @@ def precompute_g_set():
 		G_set.append(ring_set)
 		print(f"{i}/{len(RING_REGIONS)}")
 	return G_set
+
 
 def find_probablilty(player_pos: tuple, g_set, strd_dev, throws=1):
 	"""
@@ -106,24 +133,29 @@ def find_probablilty(player_pos: tuple, g_set, strd_dev, throws=1):
 	player_pos_angle = math.atan2(-player_pos[0], player_pos[1])
 	strd_dev = math.radians(strd_dev)
 	total_prob = 0
+	valid_reigons = surrounding_rings(player_displacement)
+	print(valid_reigons)
 	for i, reigon in enumerate(g_set):
-		optimal_angle = numpy.atan2(-(reigon[2]["x_coord"]-player_pos[0]), reigon[2]["z_coord"]-player_pos[1])
-		angle_diff = ((-1*optimal_angle)+player_look_angle+math.pi)%(2*math.pi)-math.pi
-		chance = (1/(strd_dev*math.sqrt(2 * math.pi)))*numpy.exp(-(numpy.pow(angle_diff, 2))/(2*pow(strd_dev, 2)))
-		placement_correction = numpy.ones(len(reigon[2]))
-		player_chunk_displacement_arr = numpy.zeros(len(reigon[2]))
-		for j, comparison_set in enumerate(g_set):
-			min_dist, max_dist, count = RING_REGIONS[j][0], RING_REGIONS[j][1], RING_REGIONS[j][2]
-			if i != j:
-				player_chunk_displacement_arr = numpy.sqrt(numpy.pow(reigon[2]["x_coord"]-player_pos[0], 2)+numpy.pow(reigon[2]["z_coord"]-player_pos[1], 2))
-				chance_closer_arr = scipy.integrate.quad_vec(diff_ring_integrand, 0, 2*math.pi, args=(player_pos_angle, player_displacement, player_chunk_displacement_arr, min_dist, max_dist), limit=10)
-				placement_correction *= numpy.pow(1 - chance_closer_arr[0], count)
-		reigon[2]["raw_weight"] *= chance * placement_correction
-		total_prob += numpy.sum(reigon[2]["raw_weight"])
+		if RING_REGIONS[i] in valid_reigons:
+			optimal_angle = numpy.atan2(-(reigon[2]["x_coord"]-player_pos[0]), reigon[2]["z_coord"]-player_pos[1])
+			angle_diff = ((-1*optimal_angle)+player_look_angle+math.pi)%(2*math.pi)-math.pi
+			chance = (1/(strd_dev*math.sqrt(2 * math.pi)))*numpy.exp(-(numpy.pow(angle_diff, 2))/(2*pow(strd_dev, 2)))
+			placement_correction = numpy.ones(len(reigon[2]))
+			player_chunk_displacement_arr = numpy.zeros(len(reigon[2]))
+
+			for j, comparison_set in enumerate(g_set):
+				min_dist, max_dist, count = RING_REGIONS[j][0], RING_REGIONS[j][1], RING_REGIONS[j][2]
+				if i != j and RING_REGIONS[j] in valid_reigons:
+					player_chunk_displacement_arr = numpy.sqrt(numpy.pow(reigon[2]["x_coord"]-player_pos[0], 2)+numpy.pow(reigon[2]["z_coord"]-player_pos[1], 2))
+					chance_closer_arr = diff_ring_integrand_simpson(player_pos_angle, player_displacement, player_chunk_displacement_arr, min_dist, max_dist)# scipy.integrate.quad_vec(diff_ring_integrand, 0, 2*math.pi, args=(player_pos_angle, player_displacement, player_chunk_displacement_arr, min_dist, max_dist), limit=10)
+					placement_correction *= numpy.pow(1 - chance_closer_arr, count)
+			reigon[2]["raw_weight"] *= chance * placement_correction
+			total_prob += numpy.sum(reigon[2]["raw_weight"])
 
 	for reigon in g_set:
 		reigon[2]["raw_weight"] /= total_prob
 	return g_set
+
 
 def extract_best(g_set):
 	flattened_gset = []
@@ -140,12 +172,14 @@ def extract_best(g_set):
 	best_val = best_coords[0][4]
 	return x,z, best_val
 
+
 def read_clipboard():
 	result = pyperclip.paste()
 	if result.startswith("/execute in minecraft"):
 		return result
 	else:
 		return "invalid"
+
 
 def parse_result(command):
 	postion_dat = command.split(" ")[-5:]
@@ -156,11 +190,14 @@ def parse_result(command):
 	pitch = float(postion_dat[4])
 	return x,y,z,yaw,pitch
 
+
 def get_gradient(pitch):
 	return -1/math.tan(math.radians(pitch))
 
+
 def get_constant(x, z, gradient):
 	return z-(gradient*x)
+
 
 def intersect_lines(g1, g2, c1, c2):
 	x = int((c2-c1)/(g1-g2))
@@ -169,8 +206,10 @@ def intersect_lines(g1, g2, c1, c2):
 	z_snap = z - (z % 16) + 8
 	return x_snap, z_snap
 
+
 def nether_coords(x, z):
 	return x//8, z//8
+
 
 def validate_result(x, z):
 	displacement = math.sqrt(x*x + z*z)
@@ -178,6 +217,7 @@ def validate_result(x, z):
 		if ring[0] < displacement < ring[1]:
 			return i+1
 	return -1
+
 
 def main():
 	while True:
@@ -212,6 +252,7 @@ def main():
 			else:
 				print(f"Likely an incorrect mesaurement")
 
+
 def main_probabilistic():
 	g_set = precompute_g_set()
 	count = 0
@@ -229,7 +270,7 @@ def main_probabilistic():
 			g_set = find_probablilty((x1, z1, yaw1), g_set, 0.0538)
 			count += 1
 			c_x, c_z, chance= extract_best(g_set)
-			print(time.time() - t1)
+			print(f"Processing time: {time.time() - t1}")
 			print(f"Overworld coords: {int(c_x), int(c_z)}")
 			print(f"Nether coords: {nether_coords(c_x, c_z)}")
 			print(chance)
@@ -239,4 +280,6 @@ def main_probabilistic():
 			else:
 				print(f"Likely an incorrect mesaurement")
 
+
 main_probabilistic()
+# print(surrounding_rings(20248.4567))
